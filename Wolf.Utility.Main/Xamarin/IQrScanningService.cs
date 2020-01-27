8fdf43401a30828413e21d2d29814b7dc3f9fe0c
André Steenhoff Madsen <andre@steenhoff.dk>
2020-01-27 14:25:51 +0100

    end of day
    created my own WebView, and some interfacases for some services i needed

---
+++ Wolf.Utility.Main/Xamarin/IQrScanningService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using ZXing.Mobile;
+
+namespace Wolf.Utility.Main.Xamarin
+{
+    public interface IQrScanningService
+    {
+        Task<string> ScanAsync(MobileBarcodeScanningOptions options);
+    }
+}
