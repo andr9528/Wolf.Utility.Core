a72c6325e361f65ecde4b0a6f5966705d2c96898
André Steenhoff Madsen <andre@steenhoff.dk>
2020-02-17 15:23:55 +0100

    end of day
    Finally managed to input the text with javascript, and click enter via javascript too.

--- Wolf.Utility.Main/Xamarin/Elements/AdvancedWebView.cs
+++ Wolf.Utility.Main/Xamarin/Elements/AdvancedWebView.cs
@@ -9,7 +9,7 @@ using XLabs.Forms.Controls;
 
 namespace Wolf.Utility.Main.Xamarin.Elements
 {
-    // Source : https://stackoverflow.com/questions/30397090/xamarin-forms-handle-clicked-event-on-webview
+    
 
     public class AdvancedWebView : HybridWebView
     {
@@ -27,7 +27,10 @@ namespace Wolf.Utility.Main.Xamarin.Elements
         public AdvancedWebView()
         {
             LoadFinished += (sender, e) =>
+            {
                 InjectJavaScript(GetJsClickEvent());
+                InjectJavaScript(GetJsEnterKeyPressEvent());
+            };
 
             RegisterCallback("invokeClick", el => {
                 var args = new ClickEvent { Element = el };
@@ -38,17 +41,97 @@ namespace Wolf.Utility.Main.Xamarin.Elements
 
         }
 
-        public void WriteToField(string fieldId, string value)
+        public void SetInputFocus(string inputId)
         {
-            var js = GetJsSetInputField(fieldId, value);
+            var js = GetJsSetFocus(inputId);
 
             InjectJavaScript(js);
 
-            Logging.Logging.Log(LogType.Information, $"Injected Javascript => {js}");
+            // Logging.Logging.Log(LogType.Information, $"Injected Javascript => {js}");
         }
-        
+
+        public void PressEnter(string inputId)
+        {
+            var js = GetJsEnterKeyPressDispatch(inputId);
+
+            InjectJavaScript(js);
+
+            // Logging.Logging.Log(LogType.Information, $"Injected Javascript => {js}");
+        }
+
+        public void WriteToField(string inputId, string value)
+        {
+            var js = GetJsSetInputField(inputId, value);
+
+            InjectJavaScript(js);
+
+            // Logging.Logging.Log(LogType.Information, $"Injected Javascript => {js}");
+        }
+
+        public void LinkInputAndButton(string inputId, string buttonId)
+        {
+            var js = GetJsLinkInputAndButton(inputId, buttonId);
+
+            InjectJavaScript(js);
+
+            // Logging.Logging.Log(LogType.Information, $"Injected Javascript => {js}");
+        }
+
 
         #region JS Strings
+        // Source: https://stackoverflow.com/questions/3276794/jquery-or-pure-js-simulate-enter-key-pressed-for-testing
+        private string GetJsEnterKeyPressDispatch(string inputId)
+        {
+            var builder = new StringBuilder();
+
+            builder.Append($"if (document.getElementById('{inputId}'))");
+            builder.Append("{");
+            builder.Append($"document.getElementById('{inputId}').dispatchEvent(ke);");
+            builder.Append("}");
+            
+            return builder.ToString();
+        }
+
+        private string GetJsEnterKeyPressEvent()
+        {
+            var builder = new StringBuilder();
+
+            builder.Append("const ke = new KeyboardEvent('keyup', {bubbles: true, cancelable: true, key: 'Enter', code: '13'});");
+
+            return builder.ToString();
+        }
+        // Source: https://stackoverflow.com/questions/155188/trigger-a-button-click-with-javascript-on-the-enter-key-in-a-text-box
+        private string GetJsLinkInputAndButton(string inputId, string buttonId)
+        {
+            var builder = new StringBuilder();
+            
+            builder.Append($"if (document.getElementById('{inputId}'))");
+            builder.Append("{");
+            builder.Append($"document.getElementById('{inputId}').addEventListener('keyup', function(event)");
+            builder.Append("{");
+            //builder.Append("event.preventDefault();");
+            builder.Append("if (event.key == 'Enter')");
+            builder.Append("{");
+            builder.Append($"document.getElementById('{buttonId}').click();");
+            builder.Append("}");
+            builder.Append("});");
+            builder.Append("}");
+
+            return builder.ToString();
+        }
+        // Source: https://stackoverflow.com/questions/17500704/javascript-set-focus-to-html-form-element
+        private string GetJsSetFocus(string inputId)
+        {
+            var builder = new StringBuilder();
+
+            builder.Append($"if (document.getElementById('{inputId}'))");
+            builder.Append("{");
+            builder.Append($"document.getElementById('{inputId}').focus();");
+            builder.Append("}");
+
+            return builder.ToString();
+        }
+        // Source: https://stackoverflow.com/questions/30397090/xamarin-forms-handle-clicked-event-on-webview
         private string GetJsClickEvent()
         {
             var builder = new StringBuilder();
@@ -64,15 +147,15 @@ namespace Wolf.Utility.Main.Xamarin.Elements
             return builder.ToString();
         }
 
-        private string GetJsSetInputField(string fieldId, string value)
+        private string GetJsSetInputField(string inputId, string value)
         {
             var builder = new StringBuilder();
 
-            //builder.Append($"if (document.getElementById(\"{fieldId}\"))");
-            //builder.Append("{");
-            builder.Append($"var input = document.getElementById(\"{fieldId}\");");
-            builder.Append($"input.setAttribute(\"value\", \"{value}\";");
-            //builder.Append("}");
+            builder.Append($"if (document.getElementById('{inputId}'))");
+            builder.Append("{");
+            builder.Append($"var input = document.getElementById('{inputId}');");
+            builder.Append($"input.setAttribute('value', '{value}');");
+            builder.Append("}");
 
             return builder.ToString();
         }
