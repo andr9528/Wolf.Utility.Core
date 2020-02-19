cfe690369765510f34e5e9dcd15102da5a3f3939
André Steenhoff Madsen <andre@steenhoff.dk>
2020-02-19 10:23:05 +0100

    Created GenericFactory.cs, wherein methods making use of the type T, and return T, can be placed.
    currently contains one method, that copies properties from one T to a new one, allowing named properties to be skipped

---
+++ Wolf.Utility.Main/Factory/GenericFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wolf.Utility.Main.Factory
+{
+    public class GenericFactory
+    {
+        // Assistance with Setter Accessibility: https://stackoverflow.com/questions/3762456/how-to-check-if-property-setter-is-public
+        public static T CopyProperties<T>(T newObject, T oldObject, bool ignoreDefaults = true,
+            bool skipSelectedProperties = true, params string[] skippedProperties) where T : class
+        {
+            var type = typeof(T);
+            var properties = type.GetProperties();
+
+            foreach (var property in properties)
+            {
+                if (ignoreDefaults && property.GetValue(oldObject) == default)
+                    continue;
+                if (skipSelectedProperties && skippedProperties.Contains(property.Name))
+                    continue;
+                if (!property.CanWrite)
+                    continue;
+                
+                property.SetValue(newObject, property.GetValue(oldObject));
+            }
+
+            return newObject;
+        }
+    }
+}
