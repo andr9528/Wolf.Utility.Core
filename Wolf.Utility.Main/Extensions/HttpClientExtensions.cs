d971c720815ac8c191ae510868fb6c4a961384c0
André Steenhoff Madsen <andre@steenhoff.dk>
2020-03-09 15:34:50 +0100

    end of day
    Added extension class for HttpClient, coping over code from DotNetCoreExam GitHub project. Added a method to allow getting via a query string in the url.
    
    Updated GenericFactory so that user can chose to go New to Old or Old to New. Old to New is needed to get a new reference in some cases, and is the default way the method runs. The New to Old way is needed when an existing reference simply needs updating.

---
+++ Wolf.Utility.Main/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Wolf.Utility.Main.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static Task<HttpResponseMessage> PostAsJsonAsync<T>(this HttpClient httpClient, string url, T data) where T : class
+        {
+            var dataAsString = JsonConvert.SerializeObject(data, Formatting.Indented,
+                new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.Objects,
+                    TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple
+                });
+
+
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            return httpClient.PostAsync(url, content);
+        }
+
+        public static Task<HttpResponseMessage> PutAsJsonAsync<T>(this HttpClient httpClient, string url, T data) where T : class
+        {
+            var dataAsString = JsonConvert.SerializeObject(data, Formatting.Indented,
+                new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.Objects,
+                    TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple
+                });
+
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            return httpClient.PutAsync(url, content);
+        }
+
+        public static Task<HttpResponseMessage> DeleteByJsonAsync<T>(this HttpClient httpClient, string url, T data) where T : class
+        {
+            var dataAsString = JsonConvert.SerializeObject(data, Formatting.Indented,
+                new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.Objects,
+                    TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple
+                });
+
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            HttpRequestMessage request = new HttpRequestMessage()
+            {
+                Content = content,
+                Method = HttpMethod.Delete,
+                RequestUri = new Uri(httpClient.BaseAddress + url)
+            };
+            return httpClient.SendAsync(request);
+        }
+
+        public static Task<HttpResponseMessage> GetByJsonAsync<T>(this HttpClient httpClient, string url, T data) where T : class
+        {
+            var dataAsString = JsonConvert.SerializeObject(data, Formatting.Indented,
+                new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.Objects,
+                    TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple
+                });
+
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            HttpRequestMessage request = new HttpRequestMessage()
+            {
+                Content = content,
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(httpClient.BaseAddress + url)
+            };
+            return httpClient.SendAsync(request);
+        }
+
+        /// <summary>
+        /// Creates a query request to the base address of the client, at the controller specified in the url input, with any 'ValueType's in the data given.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="httpClient"></param>
+        /// <param name="url"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static Task<HttpResponseMessage> GetByQueryAsync<T>(this HttpClient httpClient, string url, T data) where T : class
+        {
+            var builder = new UriBuilder($"{httpClient.BaseAddress}{url}?");
+            var type = typeof(T);
+            var properties = type.GetProperties();
+
+            foreach (var info in properties)
+            {
+                if(!info.PropertyType.IsValueType) continue;
+
+                builder.Query += $"{info.Name}={info.GetValue(data)}&";
+            }
+
+            builder.Query = builder.Query.TrimEnd('&');
+
+            HttpRequestMessage request = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = builder.Uri
+            };
+
+            return httpClient.SendAsync(request);
+        }
+    }
+}
