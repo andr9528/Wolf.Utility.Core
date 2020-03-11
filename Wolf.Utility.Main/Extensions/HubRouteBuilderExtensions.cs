467841b7e5763a6af149fbbef8b762adb45924e3
André Steenhoff Madsen <andre@steenhoff.dk>
2020-03-11 15:44:49 +0100

    end of day
    created startup module for SignalR

---
+++ Wolf.Utility.Main/Extensions/HubRouteBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Wolf.Utility.Main.Extensions
+{
+    public static class HubRouteBuilderExtensions
+    {
+        public static void MabHub<THub>(this HubRouteBuilder builder, PathString path, THub hub) where THub : Hub
+        {
+            builder.MapHub<THub>(path);
+        }
+
+        public static void MapHub<THub>(this HubRouteBuilder builder, PathString path,
+            Action<HttpConnectionDispatcherOptions> configureOptions, THub hub) where THub : Hub
+        {
+            builder.MapHub<THub>(path, configureOptions);
+        }
+
+
+    }
+}
---
+++ Wolf.Utility.Main/Startup/Modules/SignalRStartupModule.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Wolf.Utility.Main.Extensions;
+
+namespace Wolf.Utility.Main.Startup.Modules
+{
+    public class SignalRStartupModule : IStartupModule
+    {
+        private readonly List<Hub> hubs;
+
+        public SignalRStartupModule(params Hub[] hubs)
+        {
+            this.hubs = hubs.ToList();
+        }
+
+        public void SetupServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        public void ConfigureApplication(IApplicationBuilder app)
+        {
+            app.UseSignalR(route =>
+            {
+                foreach (var hub in hubs)
+                {
+                    route.MabHub($"/{hub.GetType().Name}", hub);
+                }
+            });
+        }
+    }
+}
