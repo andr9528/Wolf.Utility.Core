f61241a6a44784c3349ab8fa313fbeb3174e2421
André Steenhoff Madsen <andre@steenhoff.dk>
2020-03-16 15:32:43 +0100

    end of day
    created extension class for extensions to different collection types. Contains 1 extension for List<T> which allows for removal of an element at index and then returning the list worked on.

---
+++ Wolf.Utility.Main/Extensions/CollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wolf.Utility.Main.Extensions
+{
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// removes the element at the specified index and then returns the list worked on.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public static List<T> RemoveAtAndReturn<T>(this List<T> list, int index)
+        {
+            list.RemoveAt(index);
+            return list;
+        }
+    }
+}
