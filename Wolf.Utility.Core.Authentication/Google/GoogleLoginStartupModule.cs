4faed12987225456d83bb8627bde4210df1490a5
André Steenhoff Madsen <andre@steenhoff.dk>
2022-02-14 11:15:52 +0100

    Uploading to continue in TimeTracker

--- Wolf.Utility.Core.Authentication/Google/GoogleLoginStartupModule.cs
+++ Wolf.Utility.Core.Authentication/Google/GoogleLoginStartupModule.cs
@@ -1,21 +1,31 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+
 using Wolf.Utility.Core.Startup;
 
-namespace Wolf.Utility.Core.Authentication
+namespace Wolf.Utility.Core.Authentication.Google
 {
-    public class GoogleAuthenticationStartupModule : IStartupModule
+    public class GoogleLoginStartupModule : IStartupModule
     {
+        public GoogleLoginStartupModule()
+        {
+
+        }
+
         public void ConfigureApplication(IApplicationBuilder app)
         {
+            
         }
 
         public void SetupServices(IServiceCollection services)
         {
-            //services.AddAuthentication().AddGoogle();
+            
         }
     }
 }
